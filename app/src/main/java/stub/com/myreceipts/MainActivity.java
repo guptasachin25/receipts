26790b5338990a0878a8f37b4c4dc5c2c61a3bab
sachingupta <guptasachin25>
2015-07-05 15:06:27 -0700

    Initial Commit

--- app/src/main/java/stub/com/myreceipts/MainActivity.java
+++ app/src/main/java/stub/com/myreceipts/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // need to add some changes here
     }
 
     @Override
